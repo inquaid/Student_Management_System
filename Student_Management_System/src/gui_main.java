bd12a8c7ccf4b8ae71049cefb7ed899131e87f76
Azmain Inquaid Haque <azmaininquaidhaque@gmail.com>
2024-04-05 23:54:24 +0600

    added main_panel for GUI.

--- Student_Management_System/src/gui_main.java
+++ Student_Management_System/src/gui_main.java
@@ -2,12 +2,36 @@ import javax.swing.*;
 
 public class gui_main extends JFrame {
     private JPanel main_panel;
+    private JLabel title_1;
+    private JTextField name_text_field;
+    private JTextField address_text_field;
+    private JTextField id_text_field;
+    private JLabel name_label;
+    private JLabel address_label;
+    private JLabel id_label;
+    private JButton Update_btn;
+    private JButton Delete_btn;
+    private JButton Add_btn;
+    private JPanel button_panel;
+    private JTextField cg_text_field;
+    private JLabel cg_label;
+    private JPanel sort_panel;
+    private JButton sortButton;
+    private JComboBox comboBox1;
+    private JProgressBar progressBar1;
 
     gui_main() {
         setContentPane(main_panel);
         setTitle("Student Management System");
-        setSize(450, 300);
+        setSize(620, 300);
+        setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+        title_1.setText("Student Management System");
+        name_label.setText("Name");
+        address_label.setText("Address");
+        id_label.setText("ID");
     }
+
+
 }
